80cf5c4e42dfc49cf67f4ae77e6285128d014230
Payton <paytonjhall@gmail.com>
2023-03-10 09:29:15 -0700

    got home page and add recipe page to show recipes

--- home.js
+++ home.js
@@ -1,3 +1,7 @@
+function getUserName() {
+    return localStorage.getItem('username') ?? 'Mystery User';
+}
+
 function likeRecipe(recipeId) {
 
 }
@@ -12,3 +16,49 @@ function makeRecipeObject(name, instructions){
         instructions: instructions
     }
 }
+
+class homePage {
+    constructor() {
+        const userName = document.querySelector('.user-name');
+        userName.textContent = getUserName();
+    }
+
+    loadRecipes() {
+
+        console.log("loading recipes");
+        let recipes
+        console.log(this.usersRecipes)
+        const recipesText = localStorage.getItem('recipes');
+        if (recipesText) {
+            recipes = JSON.parse(recipesText);
+        }
+
+        const tableBodyEl = document.querySelector('#recipeList');
+
+        if (recipes && recipes.length) {
+            for (const [i, recipe] of recipes.entries()) {
+
+                const nameTdEl = document.createElement('td');
+                const instructionTdEl = document.createElement('td');
+                const authorTdEl = document.createElement('td');
+                const likesTdEl = document.createElement('td');
+
+                nameTdEl.textContent = recipe.name;
+                instructionTdEl.textContent = recipe.instructions;
+                likesTdEl.textContent = recipe.likes;
+                authorTdEl.textContent = recipe.author;
+                    const rowEl = document.createElement('tr');
+                    rowEl.appendChild(nameTdEl);
+                    rowEl.appendChild(instructionTdEl);
+                    rowEl.appendChild(authorTdEl);
+                    rowEl.appendChild(likesTdEl);
+                    tableBodyEl.appendChild(rowEl);
+            }
+        } else {
+            tableBodyEl.innerHTML = '<tr><td colSpan=4>Add a recipe!</td></tr>';
+        }
+    }
+}
+
+const home = new homePage();
+home.loadRecipes();
--- user.js
+++ user.js
@@ -3,70 +3,114 @@ function getUserName() {
 }
 
 class userProfile {
-constructor(){
-    let usersRecipes = [];
-    const userName = document.querySelector('.user-name');
-    userName.textContent = getUserName();
-    const recipeCount = document.querySelector('.recipe-count');
-    recipeCount.textContent = usersRecipes.length;
+    usersRecipes = [];
+    constructor() {
 
-}
-
-addRecipe(recipe){
-    usersRecipes.push(recipe);
-    loadRecipes();
-    recipeList = usersRecipes.length;
-}
-
-makeRecipe(){
-    const name = document.querySelector('.recipeName');
-    const instructions = document.querySelector('.recipeInstructions');
-    this.addRecipe({
-        likes: 0,
-        like: function(){
-            this.likes++;
-        },
-        name: name,
-        instructions: instructions
-        })
-    console.log("added recipe: " + name);
-    }
-
-        likeRecipe(recipe){
-        recipe.like();
-        updateCount();
-    }
-
-    updateCount(){
+        this.usersRecipes = localStorage.getItem('recipes') ?? [];
+        console.log("user profile loaded");
+        const userName = document.querySelector('.user-name');
+        userName.textContent = getUserName();
+        // localStorage.clear(); // reset local storage
         const recipeCount = document.querySelector('.recipe-count');
-        recipeCount.textContent = usersRecipes.length;
+        recipeCount.textContent = this.usersRecipes.length + "";
+    }
+
+    addRecipe(recipe) {
+        console.log("adding recipe");
+        let recipes
+        const recipesText = localStorage.getItem('recipes');
+        if (recipesText) {
+            console.log("recipes text: " + recipesText);
+            try {
+                recipes = JSON.parse(recipesText);
+            } catch (e) {
+                recipes = [];
+            }
+            console.log("recipes: " + recipes);
+        }
+        console.log(typeof recipes);
+        console.log("recipes type")
+        recipes.push(recipe);
+        //this.usersRecipes.push(recipe);
+        localStorage.setItem('recipes', JSON.stringify(recipes));
+        this.loadRecipes();
+        // recipeCount = usersRecipes.length; //Not sure what this is for
+    }
+
+    makeRecipe() {
+        console.log("making recipe");
+        let name = document.querySelector("#recipeName").value;
+        let instructions = document.querySelector("#recipeInstructions").value;
+        //document.querySelector("#recipeName").value = "";
+        //document.querySelector("#recipeInstructions").value = "";
+
+        this.addRecipe({
+            likes: 0,
+            like: function () {
+                this.likes++;
+            },
+            name: name,
+            instructions: instructions,
+            author: getUserName()
+        })
+        console.log("added recipe: " + name);
+    }
+
+    likeRecipe(recipe) {
+        //This will run when the like button is pressed
+        recipe.like();
+        this.updateCount();
+    }
+
+    updateCount() {
+        const recipeCount = document.querySelector('.recipe-count');
+        recipeCount.textContent = this.usersRecipes.length + "";
+    }
+
+
+    loadRecipes() {
+        console.log("loading recipes");
+        let recipes = []
+        const recipesText = localStorage.getItem('recipes');
+        if (recipesText) {
+            console.log("recipes text: " + recipesText);
+            try {
+                recipes = JSON.parse(recipesText);
+            } catch (e) {
+                recipes = []
+                console.log(e);
+            }
+            console.log("recipes: " + recipes);
         }
 
-    loadRecipes(){
-       for (const [i, recipe] of usersRecipes.entries()){
-           console.log(recipe);
-           const recipeCard = document.createElement('div');
-           recipeCard.classList.add('recipe-card');
-           recipeCard.setAttribute('data-index', i);
-           recipeCard.innerHTML = `
-           <div class="recipe-card">
-           <h2>${recipe.name}</h2>
-           <p>${recipe.instructions}</p>
-           <p>Likes: ${recipe.likes}</p>
-           <button class="like-btn">Like</button>
-           </div>
-           `;
-           const likeBtn = recipeCard.querySelector('.like-btn');
-           likeBtn.addEventListener('click', function(){
-               recipe.like();
-               likeBtn.previousElementSibling.textContent = `Likes: ${recipe.likes}`;
-           })
-           document.querySelector('.recipe-list').appendChild(recipeCard);
-       }
+        const tableBodyEl = document.querySelector('#recipeList');
+
+        if (recipes && recipes.length) {
+            for (const [i, recipe] of recipes.entries()) {
+
+                const nameTdEl = document.createElement('td');
+                const instructionTdEl = document.createElement('td');
+                const authorTdEl = document.createElement('td');
+                const likesTdEl = document.createElement('td');
+
+                nameTdEl.textContent = recipe.name;
+                instructionTdEl.textContent = recipe.instructions;
+                likesTdEl.textContent = recipe.likes;
+                authorTdEl.textContent = recipe.author;
+                if(recipe.author === getUserName()) {
+                    const rowEl = document.createElement('tr');
+                    rowEl.appendChild(nameTdEl);
+                    rowEl.appendChild(instructionTdEl);
+                    rowEl.appendChild(authorTdEl);
+                    rowEl.appendChild(likesTdEl);
+                    tableBodyEl.appendChild(rowEl);
+                }
+            }
+        } else {
+            tableBodyEl.innerHTML = '<tr><td colSpan=4>Add a recipe!</td></tr>';
+        }
     }
 }
-
 const user = new userProfile();
 
-
-
+user.loadRecipes();
